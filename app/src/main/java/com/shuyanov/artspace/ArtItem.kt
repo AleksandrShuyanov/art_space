47c558f334c43e18f26e1cc2b139713f45b1b10c
Aleksandr Shuyanov <info@iti52.ru>
2024-06-17 22:33:02 +0300

    Slides and navigation

---
+++ app/src/main/java/com/shuyanov/artspace/ArtItem.kt
@@ -0,0 +1,3 @@
+package com.shuyanov.artspace
+
+class ArtItem(val imagePath: Int, val title: String, val author: String, val year: Int) {}
--- app/src/main/java/com/shuyanov/artspace/MainActivity.kt
+++ app/src/main/java/com/shuyanov/artspace/MainActivity.kt
@@ -3,13 +3,36 @@ package com.shuyanov.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.shuyanov.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +45,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ViewSlide()
                 }
             }
         }
@@ -30,17 +53,171 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun ViewSlide(
+    modifier: Modifier = Modifier
+) {
+    val gallery: List<ArtItem> = listOf(
+        ArtItem(R.drawable.viktor_mihajlovich_vasnecov_osnovatel_skazochno_bylinnogo_zhanra_5ffbc47589d93, "Алёнушка", "Виктор Васнецов", 1881),
+        ArtItem(R.drawable.sery_volk, "Иван-царевич на сером волке", "Виктор Васнецов", 1889),
+        ArtItem(R.drawable.bogatiry, "Богатыри", "Виктор Васнецов", 1898),
+        ArtItem(R.drawable.tsarevna,"Сказка о спящей царевне", "Виктор Васнецов", 1926),
+    )
+
+    var itemIndex by remember {
+        mutableIntStateOf(0)
+    }
+
+    val activeArtItem = gallery[itemIndex]
+
+    Column(
+        verticalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+            .background(Color.White)
+            .fillMaxHeight()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            // Item content
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+            ) {
+                Surface(
+                    shadowElevation = 15.dp,
+                    modifier = Modifier
+                        .padding(26.dp)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(25.dp)
+                    ) {
+                        Image(
+                            painter = painterResource(id = activeArtItem.imagePath),
+                            contentDescription = activeArtItem.title,
+                            contentScale = ContentScale.Fit,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        )
+                    }
+
+                }
+
+            }
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(25.dp, 0.dp, 25.dp, 0.dp)
+            ) {
+                // Text block
+
+                Column(
+                    modifier = Modifier
+                        .background(colorResource(id = R.color.label_bg))
+                        .fillMaxWidth()
+                        .padding(15.dp)
+                ) {
+                    TextLabel(
+                        activeArtItem.title,
+                        fontWeight = FontWeight.Normal,
+                    )
+                    Row() {
+                        TextLabel(
+                            activeArtItem.author,
+                            fontWeight = FontWeight.Bold,
+                        )
+                        TextLabel(
+                            "(${activeArtItem.year})",
+                            modifier.padding(4.dp, 0.dp, 0.dp, 0.dp),
+                            fontWeight = FontWeight.Normal,
+                            fontStyle = FontStyle.Italic
+                        )
+                    }
+                }
+
+
+            }
+        }
+
+        Column(
+            modifier = modifier
+                .padding(25.dp)
+        ) {
+            // Buttons block
+
+            val horizontalArrangement = when (itemIndex) {
+                0 -> Arrangement.End
+                in 1..<gallery.size -> Arrangement.SpaceBetween
+                else -> Arrangement.Start
+            }
+
+            Row(
+                horizontalArrangement = horizontalArrangement,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                if (itemIndex > 0) {
+                    NavigateButton(
+                        btnText = stringResource(id = R.string.btn_prev),
+                        onClick = { itemIndex-- },
+                        callbackParam = "prev"
+                    )
+                }
+
+                if (itemIndex < gallery.size - 1) {
+                    NavigateButton(
+                        btnText = stringResource(id = R.string.btn_next),
+                        onClick = { itemIndex++ },
+                        callbackParam = "prev",
+                        modifier = Modifier
+                            .wrapContentWidth(Alignment.End)
+                    )
+                }
+
+            }
+        }
+    }
+}
+
+
+@Composable
+fun TextLabel(
+    text: String,
+    modifier: Modifier = Modifier,
+    fontWeight: FontWeight = FontWeight.Normal,
+    fontStyle: FontStyle = FontStyle.Normal
+) {
     Text(
-        text = "Hello $name!",
+        text = text,
+        fontWeight = fontWeight,
+        fontStyle = fontStyle,
         modifier = modifier
     )
+}
+
+@Composable
+fun NavigateButton(
+    btnText: String,
+    onClick: (String) -> Unit,
+    callbackParam: String,
+    modifier: Modifier = Modifier
+) {
+    Button(
+        onClick = { onClick(callbackParam) },
+        modifier = modifier
+    ) {
+        Text(text = btnText)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+        ViewSlide()
     }
 }
